073ebc31c3ee2b96644f9cd42ec94c982ae58132
jalil idrisov <dshalil1987@mail.ru>
2026-05-30 07:07:00 +0300

    Настроил чтение автора через python-dotenv

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello from repository!")
+import os
+from dotenv import load_dotenv
+
+# Загружаем переменные из файла .env
+load_dotenv()
+
+def print_author():
+    # Читаем значение переменной AUTHOR из окружения
+    author = os.getenv("AUTHOR")
+    print(f"Автор проекта: {author}")
+
+if __name__ == "__main__":
+    print_author()
